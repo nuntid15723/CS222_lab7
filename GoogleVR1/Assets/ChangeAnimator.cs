53612cb251b2b11352c5f43ddf46c7cd6275cc89
nuntid15723 <nuntid2543@gmail.com>
2022-02-25 05:42:35 +0700

    first commit

---
+++ GoogleVR1/Assets/ChangeAnimator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void grtAnimatorBreakdance(){
+        this.GetComponent<Animator>().Play("Breakdance Freeze Var 2");
+    }
+    public void grtAnimatorJumping(){
+        this.GetComponent<Animator>().Play("Jumping");
+    }
+    public void grtAnimatorMixamo_com(){
+        this.GetComponent<Animator>().Play("mixamo_com");
+    }
+}
---
+++ GoogleVR1/Assets/ChangeColors.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColors : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void grtRed(){
+        this.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+     public void grtBlue(){
+        this.GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+     public void grtGreen(){
+        this.GetComponent<Renderer>().material.color = Color.green;
+    }
+  
+}
